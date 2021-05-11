a4255d515d47906c17dbac8700a4b1951b55c177
Rei Matsuda <rei2219.matsuda@gmail.com>
2021-05-12 00:12:02 +0900

    set chakra theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Button, ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
-import './App.css'
+import theme from "./theme/theme";
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button colorScheme="teal">ボタン</Button>
+      <p>あいうえお</p>
     </ChakraProvider>
   )
 }
