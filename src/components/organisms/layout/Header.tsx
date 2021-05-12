e1127a0aa55964b0372582d3412ec636ec61420d
Rei Matsuda <rei2219.matsuda@gmail.com>
2021-05-13 00:05:52 +0900

    wip files

---
+++ src/components/organisms/layout/Header.tsx
@@ -0,0 +1,5 @@
+import React, { VFC, memo } from "react";
+
+export const Header: VFC = memo(() => {
+  return <header style={{width: '100%', height: '50px', backgroundColor:'teal'}}>header</header>
+});
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { memo, VFC } from "react";
 import { Route, Switch } from "react-router";
 
+import { HeaderLayout } from "../components/templates/HeaderLayout";
 import { Login } from "../components/pages/Login";
 import { Page404 } from "../components/pages/Page404";
 import { HomeRoutes } from "./HomeRoutes";
@@ -17,12 +18,13 @@ export const Router: VFC = memo(() => {
         render={({ match: { url } })=>(
         <Switch>
           {HomeRoutes.map((route) => {
-            return (<Route
-              key={route.path}
-              exact={route.exact}
-              path={`${url}${route.path}`}>
-              { route.children }
-            </Route>)
+            return (
+              <Route
+                exact={route.exact}
+                path={`${url}${route.path}`}>
+                <HeaderLayout>{ route.children }</HeaderLayout>
+              </Route>
+            )
           })}
         </Switch>
       )}/>
